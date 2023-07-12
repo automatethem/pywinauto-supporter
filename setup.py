e123fad41fa7f8628078757a481223bc6ede2229
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-07-12 10:46:44 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def requirements():
 
 setuptools.setup(
 	name='pywinauto-supporter',
-	version='0.0.2',
+	version='0.0.3',
 	description='Pywinauto supporter',
 	long_description=open('README.md').read(),
 	long_description_content_type='text/markdown',
